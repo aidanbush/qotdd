a1f655e06952af4916ff59192294c2da386fbdd9
aidan <busha2@mymacewan.ca>
2017-09-22 10:15:25 -0600

    add socket creation and setup also reworded and added to help message

--- qotdd.c
+++ qotdd.c
@@ -10,13 +10,21 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <netdb.h>
 
 /* system libraries */
+#include <sys/types.h>
+#include <sys/socket.h>
 
 /* project includes */
 #include "child_proc.h"
 
 #define EXIT_INVALID_OPT 2
+#define EXIT_SOCKET_FAIL 3
+
+#define SOCKET_RETRY 5
+#define PORT "1042"
+#define BACKLOG 5
 
 int v;
 
@@ -25,13 +33,81 @@ void print_usage(char *p_name) {
            "Implements quote of the day by forwarding the quote it retrieves\n"
            " from the given server.\n\n"
            "Options:\n"
-           "    -h  Displays this help message and exits\n"
-           "    -v  Verbose [use more than once more for move verbose]\n\n"
+           "    -h  Displays this help message and exit\n"
+           "    -v  Verbose [use more than once more for move verbose]\n"
+           "        v give basic debuging including errors\n"
+           "        vv adds logging of incoming and outgoing messages\n"
+           "        vvv adds logging of all traffic\n\n"
            "required options:\n"
            "  host  The destination of the server to forward from\n"
            "  port  The specific port to recieve the quote from\n"
            "  path  The path to the quote\n"
            "  key   The corresponding JSON key for the quote\n", p_name);
+}
+
+int server_proc(char *path, char *key) {
+    //addrinfo
+    struct addrinfo *res, hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+        .ai_flags = AI_PASSIVE | AI_V4MAPPED
+    };
+
+    int err;
+    int sfd;
+
+    //for socket retry times
+    for (int i = 0; i < SOCKET_RETRY; i++) {
+        err = getaddrinfo(NULL, PORT, &hints, &res);
+        if (err != 0)
+            continue;
+        struct addrinfo *cur;
+        //for all sockets from addrinfo
+        for (cur = res; cur != NULL; cur = cur->ai_next) {
+            //create socket
+            sfd = socket(cur->ai_family, cur->ai_socktype, cur->ai_protocol);
+
+            if (sfd == -1) {
+                if (v >= 1) perror("socket");
+                continue;
+            }
+
+            //socket options
+            int val = 1;
+            err = setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+            if (err == -1) {
+                if (v >= 1) perror("setsockopt");
+                close(sfd);
+                continue;
+            }
+
+            //bind
+            err = bind(sfd, cur->ai_addr, cur->ai_addrlen);
+            if (err == -1) {
+                if (v >= 1) perror("bind");
+                close(sfd);
+                continue;
+            }
+
+            //listen
+            err = listen(sfd, BACKLOG);
+            if (err == -1) {
+                if (v >= 1) perror("listen");
+                close(sfd);
+                continue;
+            }
+            break;
+        }
+
+        //free addinfo
+        freeaddrinfo(res);
+
+        if (cur != NULL) break;
+    }
+
+    //accept loop
+
+    return 0;
 }
 
 int main(int argc, char **argv) {
@@ -63,5 +139,5 @@ int main(int argc, char **argv) {
         exit(EXIT_INVALID_OPT);
     }
 
-    return 0;
+    return server_proc(path, key);
 }
